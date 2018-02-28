2bad453b56e190b819ad52952803ba23962914c8
SabreUrn <kammerjunkie@outlook.com>
2018-02-28 12:03:18 +0100

    adds number sender server solution

---
+++ udp-number-sender/UDPNumberReceiver/UDPNumberReceiver/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPNumberReceiver {
+    class Program {
+        static void Main(string[] args) {
+            string ip = "127.0.0.1";
+            int port = 9999;
+
+            UdpClient udpServer = new UdpClient(port);
+            IPAddress iPAddress = IPAddress.Parse(ip);
+            IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, port);
+
+            while(true) {
+                Byte[] receive = udpServer.Receive(ref iPEndPoint);
+                Console.WriteLine(Encoding.ASCII.GetString(receive));
+            }
+        }
+    }
+}
